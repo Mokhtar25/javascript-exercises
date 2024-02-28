5fcb7302183fee7c1b39899b0efa188cf4d8cbdc
mohtar25 <mokhtar2000naj@gmail.com>
2024-02-28 18:14:28 +0100

    done with 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof(num1) !== "number" ||  typeof(num2) !== "number"){
+        return "ERROR";
+    }
+    let smaller = 0;
+    let bigger = 0;
+    if (num1 > num2){
 
+     smaller = num2;
+      bigger = num1;
+    }
+    else{
+        smaller = num1;
+         bigger = num2;
+
+    } 
+    let sum = 0;
+    for (let i = smaller; i <= bigger; i++){
+        sum+=i;
+    }
+    return sum;
 };
+console.log(sumAll("123",4))
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let rest = (num - 32) * 5/9;
+  return Math.round(rest*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+    let rest = (num * 9/5 + 32);
+    return Math.round(rest * 10)/ 10;
 };
 
 // Do not edit below this line
